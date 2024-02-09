d03dd88e04bbaeb90516595563e09f1a0085359c
oguro-yuki <oguro-yuki@dmm.com>
2024-02-09 14:32:15 +0900

    mvpキャンパス用にプロンプト調整

--- dist/index.js
+++ dist/index.js
@@ -19707,24 +19707,13 @@ $file_diff
 ## Instructions
 
 I would like you to succinctly summarize the diff within 100 words.
-If applicable, your summary should include a note about alterations 
-to the signatures of exported functions, global data structures and 
-variables, and any changes that might affect the external interface or 
-behavior of the code.
+Please summarize which items in the MVP campus have been modified and how.
 `;
     triageFileDiff = `Below the summary, I would also like you to triage the diff as \`NEEDS_REVIEW\` or 
 \`APPROVED\` based on the following criteria:
 
-- If the diff involves any modifications to the logic or functionality, even if they 
-  seem minor, triage it as \`NEEDS_REVIEW\`. This includes changes to control structures, 
-  function calls, or variable assignments that might impact the behavior of the code.
-- If the diff only contains very minor changes that don't affect the code logic, such as 
-  fixing typos, formatting, or renaming variables for clarity, triage it as \`APPROVED\`.
-
-Please evaluate the diff thoroughly and take into account factors such as the number of 
-lines changed, the potential impact on the overall system, and the likelihood of 
-introducing new bugs or security vulnerabilities. 
-When in doubt, always err on the side of caution and triage the diff as \`NEEDS_REVIEW\`.
+- Any addition of text or change in the meaning of text in Diff, even if it is trivial, should be triaged as \`NEEDS_REVIEW\`.
+- If the diff contains changes that do not change the meaning of the text, such as typo corrections, formatting, etc., triage it as ‶APPROVED‶.
 
 You must strictly follow the format below for triaging the diff:
 [TRIAGE]: <NEEDS_REVIEW or APPROVED>
@@ -19733,8 +19722,7 @@ Important:
 - In your summary do not mention that the file needs a through review or caution about
   potential issues.
 - Do not provide any reasoning why you triaged the diff as \`NEEDS_REVIEW\` or \`APPROVED\`.
-- Do not mention that these changes affect the logic or functionality of the code in 
-  the summary. You must only use the triage status format above to indicate that.
+- Summarize how the MVP campus has fundamentally changed.
 `;
     summarizeChangesets = `Provided below are changesets in this pull request. Changesets 
 are in chronlogical order and new changesets are appended to the
@@ -19761,7 +19749,6 @@ Instructions:
 - Focus on summarizing only the changes in the PR and stick to the facts.
 - Do not provide any instructions to the bot on how to perform the review.
 - Do not mention that files need a through review or caution about potential issues.
-- Do not mention that these changes affect the logic or functionality of the code.
 - The summary should not exceed 500 words.
 `;
     reviewFileDiff = `## GitHub PR Title
@@ -19782,20 +19769,16 @@ $short_summary
 
 ## IMPORTANT Instructions
 
-Input: New hunks annotated with line numbers and old hunks (replaced code). Hunks represent incomplete code fragments.
-Additional Context: PR title, description, summaries and comment chains.
-Task: Review new hunks for substantive issues using provided context and respond with comments if necessary.
-Output: Review comments in markdown with exact line number ranges in new hunks. Start and end line numbers must be within the same hunk. For single-line comments, start=end line number. Must use example response format below.
-Use fenced code blocks using the relevant language identifier where applicable.
-Don't annotate code snippets with line numbers. Format and indent code correctly.
-Do not use \`suggestion\` code blocks.
-For fixes, use \`diff\` code blocks, marking changes with \`+\` or \`-\`. The line number range for comments with fix snippets must exactly match the range to replace in the new hunk.
+Input: Input: new and old hanks (replaced codes) annotated with line numbers. Hanks represent incomplete sentence fragments.
+Additional context: PR title, description, summary, comment chain.
+Task Review the substantive issues in the new hank using the context provided and address them with comments if necessary.
+Output: review comments with an accurate markdown of the range of line numbers in the new hank. The start and end line numbers must be in the same hank; for one-line comments, start = end line number. The following example response format should be used.
+Use a fenced code block with the associated language identifier, if applicable.
+Do not add line numbers to code snippets.
+For fixes, mark changes with \`+\` or \`-\` and use \`diff\` code blocks. The line number range of the comment containing the fix snippet must match exactly the range to be replaced by the new hank.
 
-- Do NOT provide general feedback, summaries, explanations of changes, or praises 
-  for making good additions. 
-- Focus solely on offering specific, objective insights based on the 
-  given context and refrain from making broad comments about potential impacts on 
-  the system or question intentions behind the changes.
+- Do not provide general feedback, summaries, descriptions of changes, or praise for making good additions.
+- Focus only on providing specific, objective insights based on the given context, and be careful to ensure that the MVP campus has all the necessary information.
 
 If there are no issues found on a line range, you MUST respond with the 
 text \`LGTM!\` for that line range in the review section. 
@@ -19806,30 +19789,19 @@ text \`LGTM!\` for that line range in the review section.
 
 ---new_hunk---
 \`\`\`
-  z = x / y
-    return z
+### ⑧今後起こり得る想定リスク
 
-20: def add(x, y):
-21:     z = x + y
-22:     retrn z
-23: 
-24: def multiply(x, y):
-25:     return x * y
+20: この変更をおこなったことによりユーザービリティが下がってユーザーが離脱するリスクがあるかもしれない。
+21: そういった場合の打ち手として、こまめに数値を計測し悪い傾向が見られたらすぐにUIを変更できるような対策を入れておく。
 
-def subtract(x, y):
-  z = x - y
+ストアからあるゲームをダウンロードしている最中の待ち時間に遊ぼうと思ったゲームがアップデートが必要だった場合、エラーになる不快感。
 \`\`\`
   
 ---old_hunk---
 \`\`\`
-  z = x / y
-    return z
+### ⑧今後起こり得る想定リスク
 
-def add(x, y):
-    return x + y
-
-def subtract(x, y):
-    z = x - y
+ストアからあるゲームをダウンロードしている最中の待ち時間に遊ぼうと思ったゲームがアップデートが必要だった場合、エラーになる不快感。
 \`\`\`
 
 ---comment_chains---
@@ -19842,7 +19814,7 @@ Please review this change.
 ### Example response
 
 22-22:
-There's a syntax error in the add function.
+
 \`\`\`diff
 -    retrn z
 +    return z
@@ -20705,7 +20677,7 @@ ${commentChain}
             if (patchesPacked > 0) {
                 // perform review
                 try {
-                    const [response] = await heavyBot.chat(prompts.renderReviewFileDiff(ins));
+                    const response = await heavyBot.chat(prompts.renderReviewFileDiff(ins));
                     if (!response) {
                         (0,core.info)('review: nothing obtained from openai');
                         reviewsFailed.push(`${filename} (no response)`);
